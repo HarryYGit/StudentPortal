c195eca6c74dea20e9d84986900fa96065beb3de
Haoran Yan <84384076+HarryYGit@users.noreply.github.com>
2024-03-14 10:10:53 +1100

    add method and Edit page to allow to edit student info from database

--- StudentPortal.Web/Controllers/StudentsController.cs
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -49,5 +49,33 @@ namespace StudentPortal.Web.Controllers
 
             return View(students); //return the collection to view
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);   
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+
+                
+
+            }
+            return RedirectToAction("List", "Students");
+        }
     }
 }
