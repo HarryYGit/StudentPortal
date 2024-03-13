b3a7607d9e60ce5fe2b68223caa16a968d788fcf
Haoran Yan <84384076+HarryYGit@users.noreply.github.com>
2024-03-14 00:05:44 +1100

    initial sql server; create controller for students table

---
+++ StudentPortal.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Models.Entities;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDbContext : DbContext // normally inherit to DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) //create constructor
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; } // particular type is student, it referencing a student collection: table name is Students
+    }
+}
